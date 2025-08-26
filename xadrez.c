a3fabbcad20817c5840f0af918ec5ee8bc575203
Otto David de Santana Freitag <149729047+OttoF77@users.noreply.github.com>
2025-08-26 15:49:20 +0000

    Altrações no código, corrigindo os movimentos das 3 primeiras peças e acrescendo o cavalo, com o seu movimento.

--- xadrez.c
+++ xadrez.c
@@ -4,7 +4,7 @@
 // Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
 // O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
 
-int main() {
+int main(){
     //Declaração de variaveis
     char mov_acima[10] = "cima";
     char mov_abaixo[10] = "baixo"; 
@@ -13,7 +13,7 @@ int main() {
     int num_mov;
     int opcao;
     int status = 1;
-    char menu[300] = "Digite a peça que deseja movimentar:\n 1 - Torre\n 2 - Bispo\n 3 - Rainha\n 4 - Sair do jogo\n";
+    char menu[300] = "Digite a peça que deseja movimentar:\n 1 - Torre\n 2 - Bispo\n 3 - Rainha\n 4 - Cavalo\n 5 - Sair do jogo\n";
     char mov_vertical[10], mov_horizontal[10];
 
     //Exibição do menu e leitura da opção
@@ -36,7 +36,7 @@ int main() {
                 printf("Movimento diagonal:\n");
                 num_mov = 5;
                 for(int i = 1; i <= num_mov; i++){
-                    printf("Passo %d: 1 casa na diagonal para cima e direita\n", i);
+                    printf("Passo %d: movimento na diagonal 1 casa para cima e 1 casa para direita\n", i);
                 }
                 break;
 
@@ -45,11 +45,25 @@ int main() {
                 printf("Movimento diagonal:\n");
                 num_mov = 8;
                 for(int i = 1; i <= num_mov; i++){
-                    printf("Passo %d: 1 casa para direita e 1 casa para cima\n", i);
+                    printf("Passo %d: 1 casa para esquerda\n", i);
                 }
                 break;
-
             case 4:
+                
+                printf("Você escolheu o cavalo\n");
+                printf("Movimento do cavalo:\n");
+                num_mov = 3;
+                for(int i = 1; i <= num_mov; i++){
+                    for(int j = 1; j <= 1; j++){
+                        if(i <= 2)
+                            printf("Passo %d: 1 casa para baixo\n", i);
+                        else
+                            printf("Passo %d: 1 casa para esquerda\n", i);
+                    }
+                }
+                break;
+                                
+            case 5:
                 printf("Saindo do jogo...\n");
                 status = 0;
                 break;
@@ -58,6 +72,7 @@ int main() {
                 printf("Opção inválida, tente novamente.\n");
         }
     }
+
     // Nível Novato - Movimentação das Peças
     // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
 
